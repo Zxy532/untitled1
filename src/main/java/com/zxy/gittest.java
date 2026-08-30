af1cdb3289c24c6836f7c3cf6cea98989e47dab8
zxy <2232028106@qq.com>
2026-08-30 20:58:37 +0800

    master commit2

--- src/main/java/com/zxy/gittest.java
+++ src/main/java/com/zxy/gittest.java
@@ -5,6 +5,7 @@ public class gittest {
         System.out.println("hello git!");
         System.out.println("hello git2!");
         System.out.println("hello git3!");
+        System.out.println("master test");
         System.out.println("hot-fix test!");
     }
 }
