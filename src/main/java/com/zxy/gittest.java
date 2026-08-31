0d32fc92539e6e5c2193bc2a3e728d5d7df1dfb7
zxy <2232028106@qq.com>
2026-08-31 10:32:40 +0800

    push commit

--- src/main/java/com/zxy/gittest.java
+++ src/main/java/com/zxy/gittest.java
@@ -9,5 +9,6 @@ public class gittest {
         System.out.println("hot-fix test!");
         System.out.println("master test2");
         System.out.println("hot-fix test2!");
+        System.out.println("push test!");
     }
 }
